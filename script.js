ea7fa782666449935c90cc43b8f06db8aa9cac91
KingOfExcuses <kingofexcusestv@gmail.com>
2023-08-18 10:22:27 -0500

    add prompt and confirms

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
+	// var password = generatePassword()
+	var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
+	const pwLength = Number(prompt('How many characters would you like your password to contain?'))
 
+	const pwLower = confirm('Would you like your password to contain LOWERCASE LETTERS?')
+
+	const pwUpper = confirm('Would you like your password to contain UPPERCASE LETTERS?')
+
+	const pwNumber = confirm('Would you like your password to contain NUMBERS?')
+
+	const pwSpecial = confirm('Would you like your password to contain SPECIAL CHARACTERS?')
+
+	passwordText.value = password
+	// Console Logs
+	console.log(pwLength)
+	console.log(pwLower)
+	console.log(pwUpper)
+	console.log(pwNumber)
+	console.log(pwSpecial)
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', generatePassword)
