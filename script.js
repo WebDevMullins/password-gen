5878946212956f08fb4d5d0e4f84b230229616ca
KingOfExcuses <kingofexcusestv@gmail.com>
2023-08-18 11:29:58 -0500

    add char lists, types, loop

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate')
-
 // Write password to the #password input
+var passwordText = document.querySelector('#password')
+
+// Character lists
+const lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz'
+const uppercaseCharacters = lowercaseCharacters.toUpperCase()
+const numberCharacters = '0123456789'
+const specialCharacters = '!@#$%^&*()_+'
+
 function generatePassword() {
-	// var password = generatePassword()
-	var passwordText = document.querySelector('#password')
 	// Prompt user for password length
-	const pwLength = Number(prompt('How many characters would you like your password to contain?'))
-	// Check to see if password length is between 8 and 128
-	if (pwLength < 8 || pwLength > 128) {
-		alert('Password length must be between 8 and 128 characters.')
+	const pwLength = Number(
+		prompt('How many characters would you like your password to contain? (8-128)')
+	)
+	// Check to see if password length is between 8 and 128 and a number
+	if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+		alert('Password length must be between 8 and 128 characters and a number.')
 		return
 	}
 	// Prompt user for password criteria
@@ -20,7 +27,24 @@ function generatePassword() {
 	const pwNumber = confirm('Would you like to include NUMBERS?')
 	const pwSpecial = confirm('Would you like to include SPECIAL CHARACTERS?')
 
+	// Character Types
+	var characterType = ''
+
+	if (pwLower) characterType += lowercaseCharacters
+	if (pwUpper) characterType += uppercaseCharacters
+	if (pwNumber) characterType += numberCharacters
+	if (pwSpecial) characterType += specialCharacters
+
+	// Generate Password
+	var password = ''
+	// Loop through password length and add random character
+	for (let i = 0; i < pwLength; i++) {
+		const randomCharacter = Math.floor(Math.random() * characterType.length)
+		password += characterType[randomCharacter]
+	}
+	// Display password
 	passwordText.value = password
+
 	// Console Logs
 	console.log(pwLength)
 	console.log(pwLower)
